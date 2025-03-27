65f7dcd6ba31a8e41cef76647175587e0490b22f
Vetrikanth1209 <vetree1209mec@gmail.com>
2025-03-27 11:18:08 +0530

    updated db cred

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { default: mongoose } = require('mongoose')
-// const uri = "mongodb+srv://dharunprakash:12345@cluster0.tttb0r3.mongodb.net/office?retryWrites=true&w=majority&appName=Cluster0";
-const uri = "mongodb://localhost:27017/office";
+const uri = "mongodb+srv://dharunprakash:12345@cluster0.tttb0r3.mongodb.net/office?retryWrites=true&w=majority&appName=Cluster0";
+// const uri = "mongodb://localhost:27017/office";
 
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
